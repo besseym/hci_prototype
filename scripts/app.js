9c385707f80cb54c2fb3657601f8f2e1b76faacc
Michael Bessey <michael.bessey@showtime.net>
2015-10-01 00:48:30 -0400

    index sass scripts

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,16 @@
-
+requirejs.config({
+    paths: {
+        d3: '/hci_prototype/bower_components/d3/d3.min'
+    }
+});
 
 require(
-    [],
+    [
+        "d3",
+        "graph"
+    ],
     function(d3) {
 
-        console.log("hello world");
+
     }
 );
